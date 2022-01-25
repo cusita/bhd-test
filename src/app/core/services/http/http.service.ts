8d7ba5d6f19b762457901eb87b04802b35c15064
Katya Alvear <kalvearg@contratista.epm.co>
2022-01-25 07:14:46 -0500

    token is added

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { StorageService } from '../storage/storage.service';
+import { STORAGE_KEY } from '../../configs';
 
 export interface Options {
   headers?: HttpHeaders;
@@ -13,7 +15,7 @@ export interface Options {
 @Injectable()
 export class HttpService {
 
-   constructor(private readonly httpClient: HttpClient) {}
+   constructor(private readonly httpClient: HttpClient, private storageService: StorageService) {}
 
    public post<T, R>(endpoint: string, params: T, hasToken = true): Promise<R> {
 
@@ -26,7 +28,7 @@ export class HttpService {
     const headers = new HttpHeaders();
     headers.set('Content-Type', 'application/json');
 
-    const token = '4cJ&PKTqSb0b3d94f92d91337842a406fffdff2df1207edbba86d06a2007693886239f371';
+    const token = this.storageService.get(STORAGE_KEY.token);
 
     if (hasToken) {
       headers.set('Authorization', `Bearer ${ token }`);
